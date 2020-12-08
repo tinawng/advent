e4358426cb598876f08cab2b39527f767af60068
Tina Wang <tina@tina.cafe>
2020-12-08 10:17:23 +0100

    🎅🏻 Day 8

---
+++ 2020/day8.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const INPUTS = JSON.parse(fs.readFileSync('day8.json'));
+
+// PART I
+exec_inst([...INPUTS], true)
+
+// PART II
+for (let i = 0; i < INPUTS.length; i++) {
+    const instruction = INPUTS[i];
+    const new_list = [...INPUTS];
+
+    if (instruction.includes("nop")) {
+        new_list[i] = instruction.replace("nop", "jmp");
+        exec_inst(new_list);
+    }
+    if (instruction.includes("jmp")) {
+        new_list[i] = instruction.replace("jmp", "nop");
+        exec_inst(new_list);
+    }
+}
+
+function exec_inst(list, ignore_fail = false) {
+    var fail = false, acc = 0;
+
+    for (let i = 0; i < list.length; i++) {
+        const [instruction, number] = list[i].split(' ');
+        list[i] = "done 0"
+
+        if (instruction == 'acc')
+            acc += parseInt(number);
+        if (instruction == 'jmp')
+            i += parseInt(number) - 1;
+        if (instruction == 'done') {
+            fail = true;
+            break;
+        }
+    }
+    if (!fail || ignore_fail)
+        console.log(acc);
+}
