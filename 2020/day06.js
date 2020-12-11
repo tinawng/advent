6bf2336dd2886dad866fdfe22834216a204eaa1d
Tina Wang <tina@tina.cafe>
2020-12-11 14:45:46 +0100

    🎅🏼 Day 6

--- 2020/day06.js
+++ 2020/day06.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
-const INPUTS = JSON.parse(fs.readFileSync('day6.json'));
+const INPUTS = JSON.parse(fs.readFileSync('day06.json'));
 
+// ✨ PART I
 var filtered = INPUTS.map(group => {
   group = group.reduce((x, y) => x + y);
   return group.split('').filter((item, pos) => group.indexOf(item) == pos).length;
 }).reduce((x, y) => x + y);
-console.log("PART I", filtered);
+console.log("✨ PART I", filtered);
 
+// ✨ PART II
 var intersected = INPUTS.map(group => group.reduce((x, y) => intersect(x, y)).length).reduce((x, y) => x + y)
-console.log("PART II", intersected);
+console.log("✨ PART II", intersected);
 
+// 💫 Functions
 function intersect(a, b) {
   a = typeof a != "object" ? a.split('') : a
   return a.filter(Set.prototype.has, new Set(b));
