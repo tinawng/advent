9b67b1f34bee91be86ff83ebbbdbfbad8391204c
Tina Wang <tina@tina.cafe>
2021-12-01 10:29:38 +0100

    🎅 Day 1 _better

--- 2021/day01.js
+++ 2021/day01.js
@@ -1,25 +1,5 @@
 const fs = require('fs');
-const INPUTS = JSON.parse(fs.readFileSync('day01.json'));
+const INPUTS = fs.readFileSync("day01.txt").toString().split("\n").map(i => +i);
 
-function countGreater(list) {
-    let count = 0;
-    for (let i = 1; i < list.length; i++) {
-        let nm = list[i - 1];
-        let n = list[i];
-        if (n > nm) count++;
-    }
-    return count;
-}
-
-console.log('✨ PART I:', countGreater(INPUTS));
-
-var grouped_inputs = [];
-var c = 0;
-for (let i = 2; i < INPUTS.length; i++) {
-    grouped_inputs.push(INPUTS[i - 2] + INPUTS[i - 1] + INPUTS[i]);
-}
-
-console.log('✨ PART II:', countGreater(grouped_inputs));
-
-
-
+console.log('✨ PART I:', INPUTS.filter((_, i, arr) => arr[i] < arr[i + 1]).length);
+console.log('✨ PART II:', INPUTS.filter((_, i, arr) => arr[i] + arr[i + 1] + arr[i + 2] < arr[i + 1] + arr[i + 2] + arr[i + 3]).length);
