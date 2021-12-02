49827b0b69215e3280fe2927dee6b7fb861ca800
Tina Wang <tina@tina.cafe>
2021-12-02 09:54:42 +0100

    🎅 Day 1 _better

--- 2021/day01.js
+++ 2021/day01.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const INPUTS = fs.readFileSync("day01.txt").toString().split("\n").map(i => +i);
 
 console.log('✨ PART I:', INPUTS.filter((_, i, arr) => arr[i] < arr[i + 1]).length);
-console.log('✨ PART II:', INPUTS.filter((_, i, arr) => arr[i] + arr[i + 1] + arr[i + 2] < arr[i + 1] + arr[i + 2] + arr[i + 3]).length);
+console.log('✨ PART II:', INPUTS.filter((_, i, arr) => arr[i] < arr[i + 3]).length);
